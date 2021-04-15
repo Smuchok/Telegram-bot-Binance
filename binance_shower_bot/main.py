612e269ab97fb19cca3052506388aaef8d4356ee
Smuchok <73115018+Smuchok@users.noreply.github.com>
2021-04-15 23:45:47 +0300

    Update main.py

--- binance_shower_bot/main.py
+++ binance_shower_bot/main.py
@@ -46,7 +46,7 @@ def log_messages(message):
     log= f"{time_sms} - {author} : {text}"
     print('\n', log)
     log += f"  | json:{message}"
-    log_to_txt(sms, 'messages.txt')
+    log_to_txt(log, 'messages.txt')
 
 # def send_mes(user_id, str):
 #     return bot.send_message(user_id, str)
@@ -206,100 +206,3 @@ test= {
         'text': 'gdfg'
         }
 }
-test_bohdan= {
-    'content_type': 'text', 
-    'id': 225, 
-    'message_id': 225, 
-    'from_user': {
-        'id': 805037677, 
-        'is_bot': False, 
-        'first_name': 'Bohdan', 
-        'username': 'Easyk3', 
-        'last_name': None, 
-        'language_code': 'uk', 
-        'can_join_groups': None, 
-        'can_read_all_group_messages': None, 
-        'supports_inline_queries': None
-        }, 
-    'date': 1618062480, 
-    'chat': {
-        'id': 805037677, 
-        'type': 'private', 
-        'title': None, 
-        'username': 'Easyk3', 
-        'first_name': 'Bohdan', 
-        'last_name': None, 
-        'photo': None, 
-        'bio': None, 
-        'description': None, 
-        'invite_link': None, 
-        'pinned_message': None, 
-        'permissions': None, 
-        'slow_mode_delay': None, 
-        'sticker_set_name': None, 
-        'can_set_sticker_set': None, 
-        'linked_chat_id': None, 
-        'location': None
-        }, 
-    'forward_from': None, 
-    'forward_from_chat': None, 
-    'forward_from_message_id': None, 
-    'forward_signature': None, 
-    'forward_sender_name': None, 
-    'forward_date': None, 
-    'reply_to_message': None, 
-    'edit_date': None, 
-    'media_group_id': None, 
-    'author_signature': None, 
-    'text': 'Ти є сам пдр', 
-    'entities': None, 
-    'caption_entities': None, 
-    'audio': None, 
-    'document': None, 
-    'photo': None, 
-    'sticker': None, 
-    'video': None, 
-    'video_note': None, 
-    'voice': None, 
-    'caption': None, 
-    'contact': None, 
-    'location': None, 
-    'venue': None, 
-    'animation': None, 
-    'dice': None, 
-    'new_chat_member': None, 
-    'new_chat_members': None, 
-    'left_chat_member': None, 
-    'new_chat_title': None, 
-    'new_chat_photo': None, 
-    'delete_chat_photo': None, 
-    'group_chat_created': None, 
-    'supergroup_chat_created': None, 
-    'channel_chat_created': None, 
-    'migrate_to_chat_id': None, 
-    'migrate_from_chat_id': None, 
-    'pinned_message': None, 
-    'invoice': None, 
-    'successful_payment': None, 
-    'connected_website': None, 
-    'reply_markup': None, 
-    'json': {
-        'message_id': 225, 
-        'from': {
-            'id': 805037677, 
-            'is_bot': False, 
-            'first_name': 'Bohdan', 
-            'username': 'Easyk3', 
-            'language_code': 'uk'
-            }, 
-        'chat': {
-            'id': 805037677, 
-            'first_name': 'Bohdan', 
-            'username': 'Easyk3', 
-            'type': 'private'
-            }, 
-        'date': 1618062480, 
-        'text': 'Ти є сам пдр'
-        }
-    }
-# print(test['json']['text'])
