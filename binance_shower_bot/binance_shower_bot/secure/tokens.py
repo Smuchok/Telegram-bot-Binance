c98f97b3c19440231b5358029d1bb5bfb44377b9
Smuchok <73115018+Smuchok@users.noreply.github.com>
2021-04-15 23:33:48 +0300

    Create tokens.py

---
+++ binance_shower_bot/binance_shower_bot/secure/tokens.py
@@ -0,0 +1,9 @@
+/binance_shower_bot/secure/tokens.py:
+def TOKEN_TELEGRAM():
+    return 'TOKEN telegram-bot-api'
+
+class TOKEN_BINANCE():
+    def API_KEY():
+        return '<TOKEN binance-api-key>'
+    def API_SECRET():
+        return 'TOKEN binance-secret-api-key'
