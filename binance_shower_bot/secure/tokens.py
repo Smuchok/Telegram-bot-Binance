5ac68711861c5517a3c829def52029c7fd8e91bc
Smuchok <73115018+Smuchok@users.noreply.github.com>
2021-04-16 12:00:45 +0300

    Update tokens.py

--- binance_shower_bot/secure/tokens.py
+++ binance_shower_bot/secure/tokens.py
@@ -1,8 +1,8 @@
 def TOKEN_TELEGRAM():
-    return 'TOKEN telegram-bot-api'
+    return '<TOKEN telegram-bot-api>'
 
 class TOKEN_BINANCE():
     def API_KEY():
         return '<TOKEN binance-api-key>'
     def API_SECRET():
-        return 'TOKEN binance-secret-api-key'
+        return '<TOKEN binance-secret-api-key>'
